eed8e541ff52f7b67b11e663c189a1461699dff6
jacobo.pitabaamonde <jpitabaamondev23@fpcoruna.afundacion.org>
2024-11-21 21:42:24 +0100

    Crear DetailActivity y conectarla a CatalogActivity

--- MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,6 +1,10 @@
 package com.example.mycatalog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CatalogActivity extends AppCompatActivity {
+    private final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.catalog_activity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button detailButton = findViewById(R.id.detailsButton);
+        detailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(context, DetailActivity.class);
+                context.startActivity(myIntent);
+            }
         });
     }
 }
